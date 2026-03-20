35b9958b1627a4b83ef9ba7fac133220fd032ddb
isarodri8914 <isarodri8914@gmail.com>
2026-03-20 11:00:30 -0600

    Descripcssssss

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -1,14 +1,3 @@
-
-function toggleMobileMenu() {
-    const menu = document.getElementById("mobileMenu");
-
-    if (menu.style.display === "flex") {
-        menu.style.display = "none";
-    } else {
-        menu.style.display = "flex";
-    }
-}
-
 function toggleMobileMenu() {
     const menu = document.getElementById("mobileMenu");
     menu.classList.toggle("active");
